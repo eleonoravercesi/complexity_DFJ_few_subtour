f0b0db55a3125e1a77652c0662a76874f0ab92f8
eleonoravercesi <eleonora.vercesi@libero.it>
2024-08-07 18:24:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# On the complexity of the Dantzig-Fulkerson-Johnson TSP formulation for few subtour constraints
+
+This is GitHub repository accompaining the paper "On the complexity of the Dantzig-Fulkerson-Johnson TSP formulation for few subtour constraints", submitted to Informs Journal on Computing and currently available as a preprint on Optimization Online.
+
+The folder contains the following files:
+
+- `tsplib_hardtsplib.ipynb`  for reproducing the results of Table 1 and 2.
+- `random.ipynb` for the results of Figure 3 and 5. 
+- `plot.ipynb` for the results of Figure 4.
+- `utils.py` containing the functions used in the notebooks, that can be of independent interest.
+
+To run the notebook, first put in the folder `data` and put there the instances of the [TSPLIB](http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/) [1] and the [HardTSPLIB](https://github.com/eleonoravercesi/HardTSPLIB/) [2].
+
+[1] Reinelt, Gerhard. "TSPLIB—A traveling salesman problem library." ORSA journal on computing 3.4 (1991): 376-384.
+
+
+[2] Vercesi, Eleonora, et al. "On the generation of metric TSP instances with a large integrality gap by branch-and-cut." Mathematical Programming Computation 15.2 (2023): 389-416.
+
+
---
+++ utils.py
@@ -0,0 +1,172 @@
+'''
+Function used to reproduce the results in "On the complexity of the Dantzig-Fulkerson-Johnson TSP formulation for few subtour constraints"
+Authors: A. Buchanan, E. Vercesi
+'''
+
+import heapq
+import gurobipy as gp
+from gurobipy import GRB
+import math
+import networkx as nx
+from itertools import combinations
+import numpy as np
+from scipy.spatial.distance import cdist
+
+def sample(n, distribution):
+    '''
+    Sample n points from a given distribution
+    :param n: Number of points to sample
+    :param distribution: Could be "uniform_points_in_unit_square", "uniform", "exponential"
+    :return: nx.Graph with the cost of the edges labelled as "cost"
+    '''
+    if distribution not in ["uniform_points_in_unit_square", "uniform", "exponential"]:
+        raise ValueError("Invalid distribution. Choose between 'uniform_points_in_unit_square', 'uniform', 'exponential'")
+    if distribution == "uniform_points_in_unit_square":
+        X = np.random.uniform(0, 1, (n, 2))
+        # Compute the euclidean distance matrix
+        D = cdist(X, X, 'euclidean')
+        # Create a graph
+        G = nx.Graph()
+        for i in range(n):
+            for j in range(i + 1, n):
+                G.add_edge(i, j, cost=round(1000*D[i, j])) # Multiply every value by 1000 and round
+        return G
+    if distribution == "uniform":
+        D = np.random.uniform(0, 1000, (n, n)) # You sample the whole matrix at once, but you only use the upper triangular part
+        G = nx.Graph()
+        for i in range(n):
+            for j in range(i + 1, n):
+                G.add_edge(i, j, cost=round(D[i, j]))  # Round to the nearest integer
+        return G
+    if distribution == "exponential":
+        D = np.random.exponential(1/999, (n, n)) # You sample the whole matrix at once, but you only use the upper triangular part
+        G = nx.Graph()
+        for i in range(n):
+            for j in range(i + 1, n):
+                G.add_edge(i, j, cost=round(1000*D[i, j]))  # Multiply every value by 1000 and round just to avoid having 0 costs
+        return G
+
+
+def is_power_of_two(n):
+    '''
+    Check if n is a power of 2
+    :param n: an integer
+    :return: True, if n is a power of 2, False otherwise
+    '''
+    return (n != 0) and (n & (n - 1) == 0)
+
+def E(G,S):
+    '''
+    Return the edges of G that are in S
+    :param G: nx.Graph
+    :param S: Could be a set or a list
+    :return: A list containing the edges of G that are in S
+    '''
+    return [(i,j) for (i,j) in G.edges if i in S and j in S]
+
+def ialg(G, verbose=False):
+    '''
+    Implement the ialg algorithm as described in the paper "On the complexity of the Dantzig-Fulkerson-Johnson TSP formulation for few subtour constraints"
+    :param G: nx.Graph, with weight on the edges labelled as "cost"
+    :param verbose: If True, print the subtours found every time a solution is found and the size of the problem every 2**n iterations
+    :return: List of list containing the subtours found and the number of subtours found
+    '''
+    m = gp.Model()
+    m.Params.OutputFlag = 0
+    x = m.addVars(G.edges, vtype=GRB.BINARY)
+
+    m.setObjective(gp.quicksum(G.edges[e]['cost'] * x[e] for e in G.edges), GRB.MINIMIZE)
+
+    # Each city should touch the tour twice (enter-and-leave)
+    m.addConstrs(gp.quicksum(x[e] for e in G.edges if e in G.edges(i)) == 2 for i in G.nodes)
+
+    m.update()
+
+    # initialize upper bound to infinity
+    incumbent = None
+    UB = math.inf
+
+    # store branch-and-bound nodes in a (min) heap
+    B = list()
+
+    # (priority=n*len(S_family)+num_comp, size=len(S_family), S_family)
+    root = (0, 0, list())
+    heapq.heappush(B, root)
+
+
+    # run branch-and-bound
+    num_nodes = 0
+
+    while len(B) > 0:
+        (priority, size, S_family) = heapq.heappop(B)
+        num_nodes += 1
+
+        if size >= UB:
+            continue  # prune by bound
+
+        # add subtour constraints
+        c = m.addConstrs(
+            gp.quicksum(x[e] for e in E(G, S_family[p])) <= len(S_family[p]) - 1 for p in range(len(S_family)))
+
+        # optimize
+        m.optimize()
+        tour_edges = [e for e in G.edges if x[e].x > 0.5]
+
+        # remove subtour constraints (so m can be re-used for later solves)
+        m.remove(c)
+        m.update()
+
+        # produces a tour?
+        if nx.is_connected(G.edge_subgraph(tour_edges)):
+            assert size == len(S_family)
+            if verbose:
+                print("Found a solution with this many subtours:", size)
+                print("Specifically, they are:")
+                for S in S_family:
+                    print(S)
+                print("S_family = ", S_family)
+            UB = size
+            incumbent = S_family.copy()
+            continue  # prune by feasibility
+
+        # prune *children* by bound
+        if size + 1 >= UB:
+            continue
+
+        # add subproblems to heap
+        sorted_components = list(sorted(nx.connected_components(G.edge_subgraph(tour_edges)), key=len))
+        num_comp = len(sorted_components)
+        indices = list(range(num_comp))
+
+        # print status update after 1, 2, 4, 8, 16, 32, ... BB nodes
+        if is_power_of_two(num_nodes) and verbose:
+            print("num_bb_nodes, num_subtour_constrs, num_conn_comp =", num_nodes, size, num_comp)
+
+        # for each subset of subtours (S_1, S_2, ..., S_t ), create a subproblem
+        #       that has a new constraint for S = S_1 \cup S_2 \cup ... \cup S_t
+        for s in range(1, num_comp):
+            comb = combinations(indices, s)
+            for subset in comb:
+
+                # create vertex subset S
+                S = list()
+                for i in subset:
+                    S += list(sorted_components[i])
+
+                    # It suffices to impose the constraint for either S or V\S.
+                # We choose to pick S with:
+                #   1. |S| < |V|/2, or
+                #   2. |S| = |V|/2 and 0 \in S
+                case1 = (2 * len(S) < G.number_of_nodes())
+                case2 = (2 * len(S) == G.number_of_nodes() and 0 in S)
+
+                if case1 or case2:
+                    S_family.append(S)
+                    priority = G.number_of_nodes() * size + num_comp
+                    new_node = (priority, size + 1, S_family.copy())
+                    heapq.heappush(B, new_node)
+                    S_family.pop()
+
+    S_family = incumbent
+    return S_family, len(S_family)
+
